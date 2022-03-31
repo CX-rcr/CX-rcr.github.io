146e0a6b331ec7799a72af7b740287bc2c3cbb5a
CX-rcr <callah@robinsoncentral.net>
2022-03-31 12:14:34 -0700

    Update events.js

--- events.js
+++ events.js
@@ -1,9 +1,19 @@
+(function() {
+    // https://dashboard.emailjs.com/admin/integration
+    emailjs.init('QT96K0HhziWwfUgrl');
+})();
+
 window.onload = function() {
-    let btn = document.getElementById("button");
-    btn.onclick = function() {
-        let name = document.getElementById("name").value;
-        let quesComm = document.getElementById("quesComm").value;
-        let email = document.getElementById("email").value;
-        alert(name + quesComm + email);
-    }
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                window.alert('SUCCESS!');
+            }, function(error) {
+                window.alert('FAILED...', error);
+            });
+    });
 }
