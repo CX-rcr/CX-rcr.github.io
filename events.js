6dc96fcf9371541b356fb990a9b74bbb81401627
CX-rcr <callah@robinsoncentral.net>
2022-04-26 13:04:10 -0700

    Update events.js

--- events.js
+++ events.js
@@ -24,23 +24,6 @@ window.onload = function() {
     f.addEventListener('submit', submitted);
 }
 
-
-
-//function search_races() {
-//	let input = document.getElementById('searchbar').value
-//	input=input.toLowerCase();
-//	let x = document.getElementsByClassName('animals');
-//	
-//	for (i = 0; i < x.length; i++) {
-//		if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//			x[i].style.display="none";
-//		}
-//		else {
-//			x[i].style.display="list-item";				
-//		}
-//	}
-//}
-
 function search_products() {
 	let p_input = document.getElementById('p_searchbar').value
 	p_input=p_input.toLowerCase();
