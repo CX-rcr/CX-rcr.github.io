d1ba006354bbc8cd8f09439ecd951b3b130aa847
CX-rcr <callah@robinsoncentral.net>
2022-04-26 12:52:33 -0700

    Update events.js

--- events.js
+++ events.js
@@ -26,20 +26,20 @@ window.onload = function() {
 
 
 
-function search_races() {
-	let input = document.getElementById('searchbar').value
-	input=input.toLowerCase();
-	let x = document.getElementsByClassName('animals');
-	
-	for (i = 0; i < x.length; i++) {
-		if (!x[i].innerHTML.toLowerCase().includes(input)) {
-			x[i].style.display="none";
-		}
-		else {
-			x[i].style.display="list-item";				
-		}
-	}
-}
+//function search_races() {
+//	let input = document.getElementById('searchbar').value
+//	input=input.toLowerCase();
+//	let x = document.getElementsByClassName('animals');
+//	
+//	for (i = 0; i < x.length; i++) {
+//		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//			x[i].style.display="none";
+//		}
+//		else {
+//			x[i].style.display="list-item";				
+//		}
+//	}
+//}
 
 function search_products() {
 	let p_input = document.getElementById('p_searchbar').value
