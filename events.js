249e97835039daae17b1df89854d26a64c34b689
CX-rcr <callah@robinsoncentral.net>
2022-04-28 11:59:56 -0700

    Update events.js

--- events.js
+++ events.js
@@ -38,3 +38,17 @@ function search_products() {
 		}
 	}
 }
+function search_races() {
+	let input = document.getElementById('searchbar').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('animals');
+	
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";				
+		}
+	}
+}
